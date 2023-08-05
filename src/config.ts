0674e27caf86ae8742a211f1c2f40e03979fb3e6
Grant Forrest <gaforres@gmail.com>
2023-08-05 15:01:51 -0400

    update description

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Grant Forrest';
-export const SITE_DESCRIPTION = 'The personal website of Grant Forrest';
+export const SITE_DESCRIPTION = 'Portfolio and technical blog';
 export const SHOW_FUTURE_POSTS = false;
